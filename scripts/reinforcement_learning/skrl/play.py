bad82e23716e6941c2de78ccb978f57c78b37734
Gabriel Dorneles <dorneles1215@gmail.com>
2026-02-18 05:05:09 -0300

    Fix import in the play.py files (#14)
    Fix #13.
    
    I was able to fix the problem in Issue #13 by changing the import.

--- scripts/reinforcement_learning/skrl/play.py
+++ scripts/reinforcement_learning/skrl/play.py
@@ -114,7 +114,7 @@ from isaaclab.envs import (
     multi_agent_to_single_agent,
 )
 from isaaclab.utils.dict import print_dict
-from isaaclab.utils.pretrained_checkpoint import get_published_pretrained_checkpoint
+from isaaclab_rl.utils.pretrained_checkpoint import get_published_pretrained_checkpoint
 
 from isaaclab_rl.skrl import SkrlVecEnvWrapper
 
