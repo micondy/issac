0db34464861c5c1d48300a61bc2115b088ce0400
scc <2637781202@qq.com>
2026-03-01 22:03:12 +0800

    widen and move mini-table farther; shift object spawn ranges

--- source/openarm/openarm/tasks/manager_based/openarm_manipulation/bimanual/lift/lift_env_cfg.py
+++ source/openarm/openarm/tasks/manager_based/openarm_manipulation/bimanual/lift/lift_env_cfg.py
@@ -51,10 +51,10 @@ class ObjectTableSceneCfg(InteractiveSceneCfg):
 
     mini_table = AssetBaseCfg(
         prim_path="{ENV_REGEX_NS}/MiniTable",
-        init_state=AssetBaseCfg.InitialStateCfg(pos=[0.42, 0.0, 0.28], rot=[0.707, 0, 0, 0.707]),
+        init_state=AssetBaseCfg.InitialStateCfg(pos=[0.50, 0.0, 0.28], rot=[0.707, 0, 0, 0.707]),
         spawn=UsdFileCfg(
             usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/SeattleLabTable/table_instanceable.usd",
-            scale=(0.70, 0.85, 0.60),
+            scale=(0.95, 1.05, 0.60),
         ),
     )
 
@@ -186,7 +186,7 @@ class EventCfg:
         func=mdp.reset_root_state_uniform,
         mode="reset",
         params={
-            "pose_range": {"x": (-0.05, 0.05), "y": (0.11, 0.23), "z": (0.22, 0.28)},
+            "pose_range": {"x": (0.00, 0.10), "y": (0.09, 0.25), "z": (0.22, 0.28)},
             "velocity_range": {},
             "asset_cfg": SceneEntityCfg("object_left", body_names="ObjectLeft"),
         },
@@ -196,7 +196,7 @@ class EventCfg:
         func=mdp.reset_root_state_uniform,
         mode="reset",
         params={
-            "pose_range": {"x": (-0.05, 0.05), "y": (-0.23, -0.11), "z": (0.22, 0.28)},
+            "pose_range": {"x": (0.00, 0.10), "y": (-0.25, -0.09), "z": (0.22, 0.28)},
             "velocity_range": {},
             "asset_cfg": SceneEntityCfg("object_right", body_names="ObjectRight"),
         },
